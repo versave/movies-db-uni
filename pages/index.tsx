c159fd944303e9c6af51427a7fde17f740bfbd20
versave <vszhivkov@gmail.com>
2021-12-11 13:03:05 +0200

    Add responsiveness and scrollTop button

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,31 @@ import classes from '../styles/Home.module.scss'
 import {Container} from "../components/Container/Container";
 import {moviesData} from "../data/movies";
 import {MovieItem} from "../components/MovieItem/MovieItem";
+import {useEffect, useState} from "react";
 
 const Home: NextPage = () => {
+    const [showScrollTopButton, setShowScrollTopButton] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenToWindowScroll);
+
+        return () => {
+            window.removeEventListener('scroll', listenToWindowScroll);
+        }
+    }, []);
+
+    const listenToWindowScroll = () => {
+        const scrollToTopThreshold = 200;
+        setShowScrollTopButton(window.scrollY >= scrollToTopThreshold);
+    }
+
+    const scrollWindowTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
+
     return (
         <div className={classes.home}>
             <Container>
@@ -15,6 +38,11 @@ const Home: NextPage = () => {
                         </div>
                     ))}
                 </div>
+
+                <button
+                    className={`${classes.scrollTop} ${showScrollTopButton ? classes.visible : ''}`}
+                    onClick={scrollWindowTop}
+                />
             </Container>
         </div>
     )
